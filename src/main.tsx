c4194894c3d700c273d6d72578aa2681ed7d4656
Yevheniia <evgenya2905@gmail.com>
2024-10-01 16:51:50 +0300

    create new branches

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,10 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
-
-
-let a = "jjj"
-
+import { createRoot } from 'react-dom/client';
+import App from './App.tsx';
+import './index.css';
+import { StrictMode } from 'react';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
-  </StrictMode>,
-)
+  </StrictMode>
+);
