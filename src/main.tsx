df3d8a93a87e2a6b97f4069a968e7bd71c76ba78
Yevheniia <evgenya2905@gmail.com>
2024-10-01 17:41:51 +0300

    create store

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,13 @@ import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
 import { StrictMode } from 'react';
+import { Provider } from 'react-redux';
+import { store } from './app/store.tsx';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </StrictMode>
 );
